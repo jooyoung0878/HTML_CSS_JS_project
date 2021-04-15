43d59532534a39ec73ed4dc4e5ec572abb6b9f35
JuJu Train <jooyoung0878@users.noreply.github.com>
2021-04-15 23:19:33 +0900

    Added main menu into the index.html
    Added some images on the main page and some javascripts to filter those images with button functions.

--- js/style.js
+++ js/style.js
@@ -96,3 +96,49 @@ $(function(){
     });
 });
 
+$(function(){
+    var $imgs = $('#gallery img');                  // Store all images
+  var $buttons = $('#buttons');                   // Store buttons element
+  var tagged = {};                                // Create tagged object
+
+  $imgs.each(function(){
+    var img = this;
+    var tags = $(this).data("tags");
+    if(tags){
+      tags.split(',').forEach(function(item){
+      if(tagged[item]==null){
+        tagged[item]=[];
+      }
+      tagged[item].push(img);
+      });  
+    }
+  });
+
+  $('<button/>',{
+    text: 'Show All'+'(' + $imgs.length + ')',
+    class: 'active',
+    click: function(){
+      $(this).addClass('active').siblings().removeClass('active');
+      $imgs.show();
+    }
+  }).appendTo($buttons);
+
+  $.each(tagged,function(tagName){
+    $('<button>',{
+      text: tagName + '(' + tagged[tagName].length + ')',
+      click: function(){
+        $(this).addClass('active').siblings().removeClass('active');
+        $imgs.hide().filter(tagged[tagName]).show();
+      }
+    }).appendTo($buttons);
+  });
+});
+
+
+
+    
+    
+  
+    
+    
+  
